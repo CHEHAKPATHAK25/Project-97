c80837e8499f80751fb64bfd46fb7500f02125ad
CHEHAKPATHAK25 <72388695+CHEHAKPATHAK25@users.noreply.github.com>
2021-08-17 15:42:13 +0530

    Add files via upload

---
+++ numberguess.py
@@ -0,0 +1,25 @@
+import random
+
+title = "Number guessing game"
+        print(title)
+
+number = int(input("Guess the number between 1 - 5 :-"))
+        print(question)
+
+if number === 5:
+        print("Congratulation! You have won")
+
+elif number === 4:
+        print("You are so close! Keep guessing until you find the correct number")
+
+elif number === 3:
+        print("Hint : If you add one plus one to me, you get the desired number. Now type out what am I")
+
+else:
+        print("Keep trying your best, that's more important being correct :)")
+
+while chances >5:
+        print("You lose :( Better luck next time!!")
+
+if not chances <5:
+        print("Oops! The number to be guessed was 5")
